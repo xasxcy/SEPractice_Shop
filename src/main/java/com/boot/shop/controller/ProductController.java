ab8ea0d55ba6b01c97f6aea01f156aa76e4ec027
xasxcy <xasxcy@gmail.com>
2021-07-12 17:22:37 +0800

    完善了/product/add页面的图片上传 完善了ProductController的logo方法

--- src/main/java/com/boot/shop/controller/ProductController.java
+++ src/main/java/com/boot/shop/controller/ProductController.java
@@ -10,6 +10,8 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
 import java.util.Optional;
 
 @Controller
@@ -56,9 +58,16 @@ public class ProductController extends BaseController{
 
     // 上传logo
     @RequestMapping("/logo")
-    public void logo(MultipartFile file){
+    public void logo(MultipartFile file, HttpServletResponse resp){
         String fileName = file.getOriginalFilename();
         System.out.println(fileName);
+        try{
+            file.transferTo(new File("/Users/xasxcy/OneDrive/create/shop/file/" + fileName)); // 注意最后一个斜杠要写，不写的话会放到上一级目录里
+        } catch (Exception e){
+            System.out.println("检查有没有创建好这个路径");
+        }
+        // 输出图片路径给页面，没有前缀，为什么？
+        outRespJson("/shop/file/" +fileName, resp);
     }
 
 }
